61c64a05f337e950446dcb3531804642475e48b1
rajvi-nar <rajvinar.333@gmail.com>
2026-04-26 23:25:17 -0700

    terraform for azure aks deployment

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  dns_prefix          = "url-shortener"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  default     = "ca8b2ecd-e3fa-443f-bccf-2553efab18f7"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-url-shortener"
+}
+
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "aks-url-shortener"
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry (globally unique, lowercase, no dashes)"
+  type        = string
+  default     = "urlshorteneracr"
+}
